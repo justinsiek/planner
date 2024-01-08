12fc490834679785ccce04837e367c1d7e19c649
Justin Siek <justinsiek05@gmail.com>
2024-01-07 18:50:49 -0800

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='flex bg-[#E7E1DD] overflow-hidden items-center justify-center h-screen w-screen'>
+    <App />
+  </div>,
+)
+ 
